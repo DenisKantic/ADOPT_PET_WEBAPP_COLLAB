03d8dc3d8d20554f799129ce020624952eeba139
lastoned <filipsarovic428@gmail.com>
2024-05-12 19:35:06 +0200

    custom register/login functionality

--- auth.ts
+++ auth.ts
@@ -2,10 +2,49 @@ import NextAuth from "next-auth";
 import {PrismaAdapter} from '@auth/prisma-adapter'
 import { PrismaClient } from "@prisma/client"
 import Google from 'next-auth/providers/google'
+import CredentialsProvider from "next-auth/providers/credentials";
+import Credentials from "@auth/core/providers/credentials"
+import { compare } from "bcrypt";
 
 const prisma = new PrismaClient()
 
 export const {handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google] 
+  pages: {
+    signIn: '/login',
+  },
+  providers: [
+    CredentialsProvider({
+        name: "Credentials",
+        credentials: {
+          email: { label: "Email", type: "email", placeholder: "youremail@mail.com" },
+          password: { label: "Password", type: "password" }
+        },
+        async authorize(credentials: any) {
+          if(!credentials?.email || !credentials?.password) {
+            throw new Error('Please enter an email and password')
+          }
+          
+          const existingUser = await prisma.user.findUnique({
+            where: { email: credentials?.email }
+          });
+          if(!existingUser) {
+            return null;
+          }
+
+          const passwordMatch = await compare(credentials.password, existingUser.password)
+
+          if(!passwordMatch) {
+            return null;
+          }
+
+          return {
+            id: `${existingUser.id}`,
+            name: existingUser.name,
+            email: existingUser.email
+          }
+        }
+      })
+],
+secret: process.env.NEXT_PUBLIC_SECRET,
 })
