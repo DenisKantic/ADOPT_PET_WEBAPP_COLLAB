9dfa223d95cf8c7ce1830e0c947d8d38447e3e3d
DenisKantic <denis.kantic18@gmail.com>
2024-05-13 01:58:30 +0200

    reverted back auth.ts file for google login and it's working

--- auth.ts
+++ auth.ts
@@ -1,12 +1,12 @@
 import NextAuth from "next-auth";
 import {PrismaAdapter} from '@auth/prisma-adapter'
 import { PrismaClient } from "@prisma/client"
-import Google from "@auth/core/providers/google"
+import Google from 'next-auth/providers/google'
 
 
 const prisma = new PrismaClient()
 
 export const {handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-  providers: [Google({ clientId: process.env.AUTH_GOOGLE_ID, clientSecret: process.env.AUTH_GOOGLE_SECRET })]
+  providers: [Google]
 })
