0181cd2d962ed52e0f3184a1c711b9640c424d3c
DenisKantic <denis.kantic18@gmail.com>
2024-05-13 23:47:36 +0200

    created adoptPet create post form with prisma functionality

--- app/createAdoptPost/page.tsx
+++ app/createAdoptPost/page.tsx
@@ -2,10 +2,40 @@ import React from 'react'
 import { Metadata } from 'next';
 import getSession from '@/lib/getSession';
 import { redirect } from 'next/navigation';
-
+import { prisma } from '@/lib/prisma'
 export const metadata: Metadata = {
     title: "Kreiraj objavu",
   };
+
+const cities = [
+    "Tuzla",
+    "Sarajevo",
+    "Zenica",
+    "Mostar",
+    "Lukavac"
+]
+
+async function createPost(formData: FormData){
+    "use server"
+
+   // const name = formData.get("name")?.toString();
+   // const imageUrl = formData.get("imageUrl")?.toString();
+    const vakcina = formData.get("vakcinisan")?.toString();
+    const cipovan = formData.get("cipovan")?.toString();
+    const pasos = formData.get("pasos")?.toString();
+    const spol = formData.get("spol")?.toString();
+    const starost = formData.get("starost")?.toString();
+
+    if(!vakcina || !cipovan || !pasos || !spol || !starost){
+        console.log("vakcina:" ,vakcina, "cipovan:", cipovan, "pasos", pasos, "spol", spol, "starost",starost )
+        throw Error("Missing required fields")
+
+    }
+
+    await prisma.adoptAnimal.create({
+        data: {vakcina, cipovan, pasos, spol, starost}
+    })
+}
 
 export default async function CreateAdoptPost() {
 
@@ -17,41 +47,100 @@ export default async function CreateAdoptPost() {
     }
   return (
     <div className='h-screen w-full bg-gray-200 px-10 py-20'>
-        <div className='w-[60%] bg-gray-100 mx-auto h-[50vh]'>
+        <div className='w-[50%] bg-gray-100 mx-auto min-h-[50vh]'>
             <h1 className='text-2xl text-center py-10 font-bold tracking-wide'>Kreiraj objavu</h1>
 
-            <form action="#" className='flex flex-col items-start'>
+            <form action={createPost} className='flex flex-col items-start w-full text-black p-5'>
 
-            <label className="text-lg">
+            {/* <label className="text-lg" htmlFor='name'>
                 Ime ljubimca
             </label>
             <input
             className="input input-bordered input-primary bg-white rounded-full mt-2 p-5 w-[50%] text-lg"
-            name="text"
-            type="ime_ljubimca"
+            name="name"
+            type='text'
             placeholder="Upišite ime ljubimca"
             required
             />
             <br />
 
-            <div className='flex items-center'>
-                <label htmlFor="">
-                    Vakcinisan: 
-                </label>
-                <button className='btn btn-outline btn-info mx-5'>DA</button>
-                <button className='btn btn-outline btn-error'>NE</button>
+            <label className="text-lg" htmlFor='imageUrl'>
+                    URL Slike
+            </label>
+            <input
+            className="input input-bordered input-primary bg-white rounded-full mt-2 p-5 w-[50%] text-lg"
+            name="imageUrl"
+            type="url"
+            placeholder="Upišite URL"
+            required
+            />
+            <br /> */}
 
-                <br />
+            <div className="flex flex-col items-start">
 
-                <label htmlFor="">
-                    Spol: 
-                </label>
-                <button className='btn btn-outline btn-info mx-5'>Muško</button>
-                <button className='btn btn-outline btn-error'>Žensko</button>
-           </div>
+                <div className='flex flex-col py-2'>
+                    <label htmlFor="vakcinisan" className="py-2">Vakcinisan:</label>
+                        <div className="flex items-center py-2">
+                            <input type="radio" name="vakcinisan" value="da" className="radio radio-info" />
+                            <label htmlFor="vakcinisan" className="ml-3">Da</label>
+                            
+                            <input type="radio" name="vakcinisan" value="ne"  className="radio radio-error ml-5" />
+                            <label htmlFor="vakcinisan" className="ml-3">Ne</label>
+                    </div>
+                </div>
+
+                <div className='flex flex-col py-2'>
+                    <label htmlFor="cipovan" className="py-2">Čipovan:</label>
+                        <div className="flex items-center py-2">
+                            <input type="radio" name="cipovan" value="da" className="radio radio-info" />
+                            <label htmlFor="cipovan" className="ml-3">Da</label>
+                            
+                            <input type="radio" name="cipovan" value="ne" className="radio radio-error ml-5" />
+                            <label htmlFor="cipovan" className="ml-3">Ne</label>
+                    </div>
+                </div>
+
+                <div className='flex flex-col py-2'>
+                    <label htmlFor="pasos" className="py-2">Pasoš:</label>
+                        <div className="flex items-center py-2">
+                            <input type="radio" name="pasos" value="da" className="radio radio-info" />
+                            <label htmlFor="pasos" className="ml-3">Da</label>
+                            
+                            <input type="radio" name="pasos" value="ne" className="radio radio-error ml-5" />
+                            <label htmlFor="pasos" className="ml-3">Ne</label>
+                    </div>
+                </div>
+
+                <div className='flex flex-col py-2'>
+                    <label htmlFor="spol" className="py-2">Spol:</label>
+                        <div className="flex items-center py-2">
+                            <input type="radio" name="spol" value="musko" className="radio radio-info" />
+                            <label htmlFor="spol" className="ml-3">Muško</label>
+                            
+                            <input type="radio" name="spol" value="zensko" className="radio radio-error ml-5" />
+                            <label htmlFor="spol" className="ml-3">Žensko</label>
+                    </div>
+                </div>
+
+                <div className='flex flex-col py-2'>
+                    <label htmlFor="starost" className="py-2">Starost:</label>
+                        <div className="flex items-center py-2">
+                            <input type="radio" name="starost" value="mladje" className="radio radio-info" />
+                            <label htmlFor="starost" className="ml-3">Mlađe</label>
+                            
+                            <input type="radio" name="starost" value="odraslo" className="radio radio-error ml-5" />
+                            <label htmlFor="starost" className="ml-3">Odraslo</label>
+
+                            <input type="radio" name="starost" value="starije" className="radio radio-error ml-5" />
+                            <label htmlFor="starost" className="ml-3">Starije</label>
+                    </div>
+                </div>
+                
+            </div>
+               
             <br />
 
-
+            <button type='submit' className='btn btn-primary mx-auto'>Submit</button>
             </form>
         </div>
     </div>
