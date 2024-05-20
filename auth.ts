50eb3634f5e942d946792fe4091bf42b2f24d4a2
DenisKantic <denis.kantic18@gmail.com>
2024-05-20 16:30:30 +0200

    accepted previous changes in auth.ts so google is only the provider

--- auth.ts
+++ auth.ts
@@ -11,46 +11,5 @@ const prisma = new PrismaClient()
 
 export const {handlers, signIn, signOut, auth } = NextAuth({
   adapter: PrismaAdapter(prisma),
-<<<<<<< HEAD
-  pages: {
-    signIn: '/login',
-  },
-  providers: [
-    CredentialsProvider({
-        name: "Credentials",
-        credentials: {
-          email: { label: "Email", type: "email", placeholder: "youremail@mail.com" },
-          password: { label: "Password", type: "password" }
-        },
-        async authorize(credentials: any) {
-          if(!credentials?.email || !credentials?.password) {
-            throw new Error('Please enter an email and password')
-          }
-          
-          const existingUser = await prisma.user.findUnique({
-            where: { email: credentials?.email }
-          });
-          if(!existingUser) {
-            return null;
-          }
-
-          const passwordMatch = await compare(credentials.password, existingUser.password)
-
-          if(!passwordMatch) {
-            return null;
-          }
-
-          return {
-            id: `${existingUser.id}`,
-            name: existingUser.name,
-            email: existingUser.email
-          }
-        }
-      })
-],
-secret: process.env.NEXT_PUBLIC_SECRET,
-})
-=======
   providers: [Google]
 })
->>>>>>> dbc1e31d930e5bcbeee749048d963abaeb7fd5ae
