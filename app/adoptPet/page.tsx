64f74b6cb920a6b8eeccdffe0366a56cffaa02c2
DenisKantic <denis.kantic18@gmail.com>
2024-06-04 21:28:35 +0200

    finished adoptPet category with dynamic url route

--- app/adoptPet/page.tsx
+++ app/adoptPet/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import CardItem from '../globalComponents/CardItem'
+import CardItem from './CardItem'
 import Image from 'next/image'
 
 export default function AdoptPets() {
