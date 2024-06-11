10336e1b61c92a81726956a8c0cb08d65a372c21
lastoned <filipsarovic428@gmail.com>
2024-06-11 13:09:54 +0200

    corrections of Login ui

--- app/(auth)/signup/components/RegisterForm.tsx
+++ app/(auth)/signup/components/RegisterForm.tsx
@@ -1,66 +1,86 @@
-"use client"
+"use client";
 
-import Input from '@/components/Input';
-import axios from 'axios';
-import { signIn, signOut } from 'next-auth/react';
-import { useRouter } from 'next/navigation';
-import React, { useEffect, useState } from 'react'
-import toast from 'react-hot-toast';
+import Input from "@/components/Input";
+import axios from "axios";
+import { signIn, signOut } from "next-auth/react";
+import { useRouter } from "next/navigation";
+import React, { useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import Image from "next/image";
 
 export default function RegisterForm() {
+  useEffect(() => {
+    signOut({
+      redirect: false,
+    });
+  }, []);
 
-    useEffect(() => {
-      signOut({
-        redirect: false,
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
+
+  const [loading, setLoading] = useState(false);
+
+  const router = useRouter();
+
+  const register = async () => {
+    setLoading(true);
+    try {
+      await axios.post("/api/register", {
+        email,
+        username,
+        password,
       });
-    }, []);
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-  
-    const [loading, setLoading] = useState(false);
+      toast.success("Successfully registered");
 
-    const router = useRouter();
-  
-    const register = async () => {
-      setLoading(true);
-      try{
-        await axios.post("/api/register", {
-          email, password
-        });
-
-        toast.success("Successfully registered");
-
-        router.push("/signin");
-      } catch(err: any){
-        console.log(err);
-        toast.error(err?.response?.data)
-      } finally{
-        setLoading(false);
-      }
+      router.push("/signin");
+    } catch (err: any) {
+      console.log(err);
+      toast.error(err?.response?.data);
+    } finally {
+      setLoading(false);
     }
+  };
 
-    return (
-      <div className='space-y-5 flex flex-col items-center'>
-        <Input 
-          label='Email'
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          disabled={loading}
+  return (
+    <div className="space-y-5 flex flex-col items-center">
+      <Input
+        label="Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        disabled={loading}
+      />
+      <Input
+        label="Username"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        disabled={loading}
+        type="text"
+      />
+      <Input
+        label="Password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        disabled={loading}
+        type="password"
+      />
+      <div className="absolute left-3 bottom-1">
+        <Image
+          src="/images/paw.png"
+          alt="paw-img"
+          height={30}
+          width={40}
+          className="p-2"
         />
-        <Input 
-          label='Password'
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          disabled={loading}
-          type='password'
-        />
-        <div
-          onClick={register}
-          className='px-10 py-3 bg-neutral-900 rounded-full text-white disabled:opacity-70 cursor-pointer'
-        >
-          Register
-        </div>
-      </div>  
-    )
+      </div>
+      <div
+        onClick={register}
+        className="btn bg-[#2F5382] text-lg text-white  border-[#2F5382] rounded-full w-full mt-5
+                 hover:bg-white hover:text-[#2F5382]"
+      >
+        Kreiraj Profil
+      </div>
+    </div>
+  );
 }
