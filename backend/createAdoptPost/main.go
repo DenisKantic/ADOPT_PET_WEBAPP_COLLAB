28c85be02a7cfcee80c207eb8ba5a3cf151a35cd
DenisKantic <denis.kantic18@gmail.com>
2024-08-09 22:57:26 +0200

    added functionalities for uploading adoptPost. Pushing it so I can continue testing on PC

--- backend/createAdoptPost/main.go
+++ backend/createAdoptPost/main.go
@@ -2,8 +2,10 @@ package createAdoptPost
 
 import (
 	"database/sql"
+	"encoding/json"
 	"fmt"
 	"github.com/joho/godotenv"
+	_ "github.com/lib/pq"
 	"log"
 	"net/http"
 	"os"
@@ -23,7 +25,7 @@ var (
 // function for reading the .env variables
 func init() {
 
-	err := godotenv.Load(".env")
+	err := godotenv.Load()
 	if err != nil {
 		fmt.Println("Error loading .env file")
 		log.Fatal("Error loading .env file")
@@ -35,6 +37,9 @@ func init() {
 	DB_NAME = os.Getenv("DB_NAME")
 	DB_HOST = os.Getenv("DB_HOST")
 	DB_PORT = os.Getenv("DB_PORT")
+
+	fmt.Printf("DB_USER: %s\nDB_PASSWORD: %s\nDB_NAME: %s\nDB_HOST: %s\nDB_PORT: %s\n", DB_USER, DB_PASSWORD, DB_NAME, DB_HOST, DB_PORT)
+
 }
 
 // function for connecting to the database
@@ -46,7 +51,7 @@ func dbConnect() (*sql.DB, error) {
 
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
-		return nil, fmt.Errorf("Error oppening connection %w", err)
+		return nil, fmt.Errorf("error oppening connection %w", err)
 	}
 	return db, nil
 }
@@ -67,7 +72,7 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 	}
 
 	// extracting form values
-	images := r.MultipartForm.File["images"]
+	//images := r.MultipartForm.File["images"]
 	category := r.FormValue("category")
 	petName := r.FormValue("petName")
 	phoneNumber := r.FormValue("phoneNumber")
@@ -86,10 +91,10 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	if len(images) == 0 {
-		http.Error(w, "No images uploaded", http.StatusBadRequest)
-		return
-	}
+	//if len(images) == 0 {
+	//	http.Error(w, "No images uploaded", http.StatusBadRequest)
+	//	return
+	//}
 
 	// creating slug from the pets name for dynamic route
 	var slug = strings.ToLower(petName)
@@ -97,6 +102,87 @@ func CreatePost(w http.ResponseWriter, r *http.Request) {
 	re := regexp.MustCompile(`[^a-z0-9-]`)
 	slug = re.ReplaceAllString(slug, "")
 
+	// opening images, saving in file server, retrieving paths and storing in database
+	var filePaths []string
+
+	//for i, file := range images {
+	//	// open the uploaded images
+	//	src, err := file.Open()
+	//	if err != nil {
+	//		http.Error(w, "Error opening the images", http.StatusInternalServerError)
+	//		return
+	//	}
+	//
+	//	defer src.Close()
+	//
+	//	// renaming images with petName(i+1)
+	//	/* this is used to avoid errors if image names
+	//	contains spaces, it could cause images to not load
+	//	on client side
+	//	*/
+	//	fileExtension := filepath.Ext(file.Filename)
+	//	newFileName := fmt.Sprintf("%s%d%s", petName, i+1, fileExtension)
+	//
+	//	// create a new file in the server
+	//	dstPath := filepath.Join("adoptImages", newFileName)
+	//	dst, err := os.Create(dstPath)
+	//	if err != nil {
+	//		http.Error(w, "Error creating a new file in the server", http.StatusInternalServerError)
+	//		return
+	//	}
+	//
+	//	defer dst.Close()
+	//
+	//	// copy the uploaded file to the server
+	//	_, err = io.Copy(dst, src)
+	//	if err != nil {
+	//		http.Error(w, "Error uploading file in the server", http.StatusInternalServerError)
+	//		return
+	//	}
+	//
+	//	// collect file paths
+	//	filePaths = append(filePaths, dstPath)
+	//
+	//} // end of the for loop for images
+	//
+	//// separating images (if it has 2 or more) with commas
+	//filePathsWithCommas := "{" + strings.Join(filePaths, ",") + "}"
+	//fmt.Println("\nNEW FILE NAMES IN ARRAY", filePathsWithCommas)
+	err = SaveToDB(category, petName, phoneNumber, description, vakcinisan, cipovan, pasos, spol, starost, location, slug)
+	if err != nil {
+		http.Error(w, fmt.Sprintf("ERROR %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	response := map[string]interface{}{
+		"message":    "Files uploaded and paths stored successfully",
+		"file_paths": filePaths,
+	}
 	w.Header().Set("Content-Type", "application/json")
 	w.WriteHeader(http.StatusOK)
+	if err := json.NewEncoder(w).Encode(response); err != nil {
+		http.Error(w, "Error encoding response", http.StatusInternalServerError)
+		return
+	}
+}
+
+func SaveToDB(category string,
+	petName string, phoneNumber string, description string,
+	vakcinisan string, cipovan string,
+	pasos string, spol string,
+	starost string, location string, slug string) error {
+
+	db, err := dbConnect()
+	if err != nil {
+		return fmt.Errorf("error occurred on server: %v", err)
+	}
+	defer db.Close()
+
+	//query := "INSERT INTO adoptPost (filePaths, category, petName, phoneNumber, description, vakcinisan, cipovan, pasos,spol, starost, location, slug) VALUES ($1, $2, $3, $4, $5,$6,$7,$8,$9,$10,$11,$12)"
+	_, err = db.Exec("INSERT INTO adoptPost ( category, petName, phoneNumber, description, vakcinisan, cipovan, pasos,spol, starost, location, slug) VALUES ($1, $2, $3, $4, $5,$6,$7,$8,$9,$10,$11)", category, petName, phoneNumber, description, vakcinisan, cipovan, pasos, spol, starost, location, slug)
+	if err != nil {
+		return fmt.Errorf("error occured on the server side: %v", err)
+	}
+
+	return nil
 }
